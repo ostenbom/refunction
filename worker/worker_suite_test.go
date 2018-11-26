b7ccbc03f783683e05897c99e52d4c0ca928b56e
Oliver Stenbom <oliver@stenbom.eu>
2018-11-26 11:05:22 +0000

    Start containerd if socket not available

--- worker/worker_suite_test.go
+++ worker/worker_suite_test.go
@@ -6,27 +6,42 @@ import (
 	"syscall"
 	"testing"
 
+	"github.com/containerd/containerd"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
 func TestWorker(t *testing.T) {
-	containerd, err := StartContainerd()
+	var createContainerd bool
+	client, err := containerd.New("/run/containerd/containerd.sock")
 	if err != nil {
-		panic("Failed to start containerd server")
+		createContainerd = true
+	} else {
+		client.Close()
+		createContainerd = false
+	}
+
+	var server *exec.Cmd
+	if createContainerd {
+		server, err = StartContainerd()
+		if err != nil {
+			panic("Failed to start containerd server")
+		}
 	}
 
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Worker Suite")
 
-	err = containerd.Process.Signal(syscall.SIGINT)
-	if err != nil {
-		panic("Could not kill containerd server")
-	}
+	if createContainerd {
+		err = server.Process.Signal(syscall.SIGINT)
+		if err != nil {
+			panic("Could not kill containerd server")
+		}
 
-	_, err = containerd.Process.Wait()
-	if err != nil {
-		panic("Containerd server did not stop")
+		_, err = server.Process.Wait()
+		if err != nil {
+			panic("Containerd server did not stop")
+		}
 	}
 }
 
