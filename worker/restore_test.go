d8a36344fcf80055d65a6ee340ee4176bf8532da
Oliver Stenbom <oliver@stenbom.eu>
2019-01-30 16:03:43 +0000

    restore_test refactor

--- worker/restore_test.go
+++ worker/restore_test.go
@@ -21,14 +21,12 @@ var _ = Describe("Worker Restoring", func() {
 		id = strconv.Itoa(GinkgoParallelNode())
 	})
 
-	Describe("changing the stack", func() {
+	Describe("state restoring", func() {
 		var worker *Worker
 		var targetLayer string
 
-		BeforeEach(func() {
+		JustBeforeEach(func() {
 			var err error
-			targetLayer = "forloopfile"
-
 			worker, err = NewWorker(id, client, targetLayer)
 			Expect(err).NotTo(HaveOccurred())
 			Expect(worker.Start()).To(Succeed())
@@ -39,153 +37,100 @@ var _ = Describe("Worker Restoring", func() {
 			Expect(err).NotTo(HaveOccurred())
 		})
 
-		It("can restore dirty stack pages", func() {
-			countLocation := getRootfs(worker, targetLayer) + "count.txt"
+		Context("when the program changes stack variables", func() {
+			BeforeEach(func() {
+				targetLayer = "forloopfile"
+			})
 
-			Eventually(func() bool {
-				_, err := os.Stat(countLocation)
-				return os.IsNotExist(err)
-			}).Should(BeFalse())
+			It("can restore dirty stack pages", func() {
+				countLocation := getRootfs(worker, targetLayer) + "count.txt"
 
-			Expect(worker.Attach()).To(Succeed())
-			defer worker.Detach()
+				Eventually(func() bool {
+					_, err := os.Stat(countLocation)
+					return os.IsNotExist(err)
+				}).Should(BeFalse())
 
-			// Work out what will be printed next
-			countContent, err := ioutil.ReadFile(countLocation)
-			Expect(err).NotTo(HaveOccurred())
-			lines := strings.Split(string(countContent), "\n")
-			lastLine := lines[len(lines)-2]
-			lastLineItems := strings.Split(lastLine, " ")
-			number, err := strconv.Atoi(lastLineItems[len(lastLineItems)-1])
-			Expect(err).NotTo(HaveOccurred())
-			incrementedLine := fmt.Sprintf("at: %d", number+1)
+				Expect(worker.Attach()).To(Succeed())
+				defer worker.Detach()
 
-			// Get first state
-			state, err := worker.GetState()
-			Expect(err).NotTo(HaveOccurred())
-			Expect(state.SavePages("[stack]")).To(Succeed())
-			Expect(worker.ClearMemRefs()).To(Succeed())
-			Expect(worker.Continue()).To(Succeed())
+				// Work out what will be printed next
+				countContent, err := ioutil.ReadFile(countLocation)
+				Expect(err).NotTo(HaveOccurred())
+				lines := strings.Split(string(countContent), "\n")
+				lastLine := lines[len(lines)-2]
+				lastLineItems := strings.Split(lastLine, " ")
+				number, err := strconv.Atoi(lastLineItems[len(lastLineItems)-1])
+				Expect(err).NotTo(HaveOccurred())
+				incrementedLine := fmt.Sprintf("at: %d", number+1)
 
-			// Let run, restore
-			time.Sleep(time.Millisecond * 60)
-			Expect(worker.Stop()).To(Succeed())
-			err = state.RestoreDirtyPages("[stack]")
-			Expect(err).NotTo(HaveOccurred())
-			Expect(worker.Continue()).To(Succeed())
+				// Get first state
+				state, err := worker.GetState()
+				Expect(err).NotTo(HaveOccurred())
+				Expect(state.SavePages("[stack]")).To(Succeed())
+				Expect(worker.ClearMemRefs()).To(Succeed())
+				Expect(worker.Continue()).To(Succeed())
 
-			// Let run, check variable was restored
-			time.Sleep(time.Millisecond * 60)
-			Expect(worker.Stop()).To(Succeed())
-			countContent, err = ioutil.ReadFile(countLocation)
-			Expect(err).NotTo(HaveOccurred())
-			numberPrintedIncrements := strings.Count(string(countContent), incrementedLine)
-			Expect(numberPrintedIncrements).To(Equal(2))
+				// Let run, restore
+				time.Sleep(time.Millisecond * 60)
+				Expect(worker.Stop()).To(Succeed())
+				err = state.RestoreDirtyPages("[stack]")
+				Expect(err).NotTo(HaveOccurred())
+				Expect(worker.Continue()).To(Succeed())
+
+				// Let run, check variable was restored
+				time.Sleep(time.Millisecond * 60)
+				Expect(worker.Stop()).To(Succeed())
+				countContent, err = ioutil.ReadFile(countLocation)
+				Expect(err).NotTo(HaveOccurred())
+				numberPrintedIncrements := strings.Count(string(countContent), incrementedLine)
+				Expect(numberPrintedIncrements).To(Equal(2))
+			})
 		})
 
-	})
+		Context("when the program changes register variables", func() {
+			BeforeEach(func() {
+				targetLayer = "forloopregisterfile"
+			})
 
-	Describe("changing registers", func() {
-		var worker *Worker
-		var targetLayer string
+			It("can restore variable in a register", func() {
+				countLocation := getRootfs(worker, targetLayer) + "count.txt"
 
-		BeforeEach(func() {
-			var err error
-			targetLayer = "forloopregisterfile"
+				Eventually(func() bool {
+					_, err := os.Stat(countLocation)
+					return os.IsNotExist(err)
+				}).Should(BeFalse())
 
-			worker, err = NewWorker(id, client, targetLayer)
-			Expect(err).NotTo(HaveOccurred())
-			Expect(worker.Start()).To(Succeed())
+				Expect(worker.Attach()).To(Succeed())
+				defer worker.Detach()
+
+				// Work out what will be printed next
+				countContent, err := ioutil.ReadFile(countLocation)
+				Expect(err).NotTo(HaveOccurred())
+				lines := strings.Split(string(countContent), "\n")
+				lastLine := lines[len(lines)-2]
+				lastLineItems := strings.Split(lastLine, " ")
+				number, err := strconv.Atoi(lastLineItems[len(lastLineItems)-1])
+				Expect(err).NotTo(HaveOccurred())
+				incrementedLine := fmt.Sprintf("at: %d", number+1)
+
+				// Get first state
+				state, err := worker.GetState()
+				Expect(err).NotTo(HaveOccurred())
+				Expect(worker.Continue()).To(Succeed())
+
+				// Let run, restore
+				time.Sleep(time.Millisecond * 60)
+				err = worker.SetRegs(state)
+				Expect(err).NotTo(HaveOccurred())
+
+				// Let run, check variable was restored
+				time.Sleep(time.Millisecond * 60)
+				Expect(worker.Stop()).To(Succeed())
+				countContent, err = ioutil.ReadFile(countLocation)
+				Expect(err).NotTo(HaveOccurred())
+				numberPrintedIncrements := strings.Count(string(countContent), incrementedLine)
+				Expect(numberPrintedIncrements).To(Equal(2))
+			})
 		})
-
-		AfterEach(func() {
-			err := worker.End()
-			Expect(err).NotTo(HaveOccurred())
-		})
-
-		It("can not restore variable in a register by memory", func() {
-			countLocation := getRootfs(worker, targetLayer) + "count.txt"
-
-			Eventually(func() bool {
-				_, err := os.Stat(countLocation)
-				return os.IsNotExist(err)
-			}).Should(BeFalse())
-
-			Expect(worker.Attach()).To(Succeed())
-			defer worker.Detach()
-
-			// Work out what will be printed next
-			countContent, err := ioutil.ReadFile(countLocation)
-			Expect(err).NotTo(HaveOccurred())
-			lines := strings.Split(string(countContent), "\n")
-			lastLine := lines[len(lines)-2]
-			lastLineItems := strings.Split(lastLine, " ")
-			number, err := strconv.Atoi(lastLineItems[len(lastLineItems)-1])
-			Expect(err).NotTo(HaveOccurred())
-			incrementedLine := fmt.Sprintf("at: %d", number+1)
-
-			// Get first state
-			state, err := worker.GetState()
-			Expect(err).NotTo(HaveOccurred())
-			Expect(state.SavePages("[stack]")).To(Succeed())
-			Expect(worker.ClearMemRefs()).To(Succeed())
-			Expect(worker.Continue()).To(Succeed())
-
-			// Let run, restore
-			time.Sleep(time.Millisecond * 60)
-			Expect(worker.Stop()).To(Succeed())
-			err = state.RestoreDirtyPages("[stack]")
-			Expect(err).NotTo(HaveOccurred())
-			Expect(worker.Continue()).To(Succeed())
-
-			// Let run, check variable was restored
-			time.Sleep(time.Millisecond * 60)
-			Expect(worker.Stop()).To(Succeed())
-			countContent, err = ioutil.ReadFile(countLocation)
-			Expect(err).NotTo(HaveOccurred())
-			numberPrintedIncrements := strings.Count(string(countContent), incrementedLine)
-			Expect(numberPrintedIncrements).To(Equal(1))
-		})
-
-		It("can restore variable in a register by registers", func() {
-			countLocation := getRootfs(worker, targetLayer) + "count.txt"
-
-			Eventually(func() bool {
-				_, err := os.Stat(countLocation)
-				return os.IsNotExist(err)
-			}).Should(BeFalse())
-
-			Expect(worker.Attach()).To(Succeed())
-			defer worker.Detach()
-
-			// Work out what will be printed next
-			countContent, err := ioutil.ReadFile(countLocation)
-			Expect(err).NotTo(HaveOccurred())
-			lines := strings.Split(string(countContent), "\n")
-			lastLine := lines[len(lines)-2]
-			lastLineItems := strings.Split(lastLine, " ")
-			number, err := strconv.Atoi(lastLineItems[len(lastLineItems)-1])
-			Expect(err).NotTo(HaveOccurred())
-			incrementedLine := fmt.Sprintf("at: %d", number+1)
-
-			// Get first state
-			state, err := worker.GetState()
-			Expect(err).NotTo(HaveOccurred())
-			Expect(worker.Continue()).To(Succeed())
-
-			// Let run, restore
-			time.Sleep(time.Millisecond * 60)
-			err = worker.SetRegs(state)
-			Expect(err).NotTo(HaveOccurred())
-
-			// Let run, check variable was restored
-			time.Sleep(time.Millisecond * 60)
-			Expect(worker.Stop()).To(Succeed())
-			countContent, err = ioutil.ReadFile(countLocation)
-			Expect(err).NotTo(HaveOccurred())
-			numberPrintedIncrements := strings.Count(string(countContent), incrementedLine)
-			Expect(numberPrintedIncrements).To(Equal(2))
-		})
-
 	})
 })
